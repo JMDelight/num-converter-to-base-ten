8601cee8abffdb074b166d1b77f09926bd8c2d0c
Alan Denison and Joel Delight <joel@>
2016-06-09 14:53:15 -0700

    add the framework to allow our number converter to work into base 36.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
-var converter = function(input) {
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var alphabetValue = ["11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36"]
+
+var converter = function(input, base) {
   array = input.split("");
   var result = 0;
   for(i = 0 ; i < array.length; i ++) {
-    var result = result + Math.pow(2, i) * parseInt(array[array.length - 1 - i]);
+    var result = result + Math.pow(base, i) * parseInt(array[array.length - 1 - i]);
   };
   return result;
 };
@@ -12,7 +15,8 @@ $(document).ready(function() {
   $(".converter-form").submit(function(event) {
     event.preventDefault();
     var userInput = ($("#user-input").val());
-    var output = converter(userInput);
+    var userBase = ($("#user-base").val());
+    var output = converter(userInput, userBase);
     $("#result").text(output);
   });
 });
