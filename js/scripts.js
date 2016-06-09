540d7ffeeee2ea98147d00773759fd4d55f7ee5b
Alan Denison and Joel Delight <alan@>
2016-06-09 14:36:58 -0700

    complete our bdd goals for binary converter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 var converter = function(input) {
   array = input.split("");
-  var result = 2 * parseInt(array[0]) + parseInt(array[1]);
-  return result
-}
+  var result = 0;
+  for(i = 0 ; i < array.length; i ++) {
+    var result = result + Math.pow(2, i) * parseInt(array[array.length - 1 - i]);
+  };
+  return result;
+};
 
 
 $(document).ready(function() {
